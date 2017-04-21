82541303461826961102e36ca93b7df5634fc18d
Luc Saffre <luc.saffre@gmail.com>
2017-04-21 23:03:16 +0300

    http://luc.lino-framework.org/blog/2017/0421.html

--- lino_vilma/__init__.py
+++ lino_vilma/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 # Copyright 2014-2017 Luc Saffre
 # License: BSD (see file COPYING for details)
-"""This is the main module of Lino Noi.
+"""This is the main module of Lino Vilma.
 
 .. autosummary::
    :toctree:
@@ -17,6 +17,6 @@ exec(compile(open(fn, "rb").read(), fn, 'exec'))
 
 __version__ = SETUP_INFO['version']
 
-intersphinx_urls = dict(docs="http://noi.lino-framework.org")
-srcref_url = 'https://github.com/lsaffre/noi/blob/master/%s'
+intersphinx_urls = dict(docs="http://vilma.lino-framework.org")
+srcref_url = 'https://github.com/lino-framework/vilma/blob/master/%s'
 
