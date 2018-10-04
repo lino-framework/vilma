02bf5d151e9154cd7c8d98f2e56f1d1d7f1e41df
Luc Saffre <luc.saffre@gmail.com>
2018-10-05 00:09:19 +0300

    http://luc.lino-framework.org/blog/2018/1004.html

--- lino_vilma/lib/vilma/help_texts.py
+++ lino_vilma/lib/vilma/help_texts.py
@@ -5,6 +5,8 @@ from django.utils.translation import ugettext_lazy as _
 
 help_texts = {
     'lino_vilma.lib.vilma.migrate.Migrator' : _("""The standard migrator for noi."""),
+    'lino_vilma.lib.vilma.migrate.Migrator.migrate_from_1_0_1' : _("""Move Deployment and Milestone from 'tickets' to new plugin
+'deploy'."""),
     'lino_vilma.lib.vilma.user_types.EndUser' : _("""An end user is somebody who uses our database, but won't work
 on it."""),
     'lino_vilma.lib.vilma.user_types.Collector' : _("""A collector is somebody who collects data into the database."""),
