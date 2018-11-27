dec99c37a3992968731dd2cd9e7bcb21b4234066
Luc Saffre <luc.saffre@gmail.com>
2018-11-27 14:43:30 +0200

    copyright headers

--- lino_vilma/lib/vilma/models.py
+++ lino_vilma/lib/vilma/models.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2014-2016 Luc Saffre
+# Copyright 2014-2016 Rumma & Ko Ltd
 # License: BSD (see file COPYING for details)
 """The :xfile:`models.py` module for :mod:`lino_noi`.
 
