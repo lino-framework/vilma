7f32616df04ba1ff9b8f873e28bd6bf8ab0bf27d
Luc Saffre <luc.saffre@gmail.com>
2021-04-01 22:15:14 +0300

    https://luc.lino-framework.org/blog/2021/0401.html

--- lino_vilma/lib/vilma/models.py
+++ lino_vilma/lib/vilma/models.py
@@ -32,6 +32,5 @@ def my_details(sender, **kw):
     sender.modules.system.SiteConfigs.set_detail_layout("""
     site_company next_partner_id:10 default_build_method
     # site_calendar simulate_today hide_events_before
-    # default_event_type max_auto_events 
-    """)
-
+    # default_event_type max_auto_events
+    """, window_size=(60, 'auto'))
