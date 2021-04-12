07b586104d8738d83b7d3c2b7f600a5ffe43a78d
Luc Saffre <luc.saffre@gmail.com>
2021-04-12 21:42:32 +0300

    deactivate build docs

--- lino_vilma/__init__.py
+++ lino_vilma/__init__.py
@@ -15,6 +15,6 @@ from .setup_info import SETUP_INFO
 
 __version__ = SETUP_INFO['version']
 
-intersphinx_urls = dict(docs="http://vilma.lino-framework.org")
-srcref_url = 'https://github.com/lino-framework/vilma/blob/master/%s'
-doc_trees = ['docs']
+# intersphinx_urls = dict(docs="http://vilma.lino-framework.org")
+# srcref_url = 'https://github.com/lino-framework/vilma/blob/master/%s'
+doc_trees = []
